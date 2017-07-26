5b0100d0553e245cb2ea2d987be94c054b5e8040
kaneta <kaneta1011@gmail.com>
2017-07-26 23:31:22 +0900

    gofmt

--- imap/imap.go
+++ imap/imap.go
@@ -1,194 +1,195 @@
 package imap
+
 import (
-    "crypto/tls"
-    "bufio"
-    "io"
-    "log"
-    "strings"
-    "fmt"
+	"bufio"
+	"crypto/tls"
+	"fmt"
+	"io"
+	"log"
+	"strings"
 )
 
 type Imap struct {
-    addr        string
-    userId      string
-    passward    string
-    mailBox     string
-    conn        *tls.Conn
-    response    chan string
-    r           *bufio.Reader
-    w           io.Writer
+	addr     string
+	userId   string
+	passward string
+	mailBox  string
+	conn     *tls.Conn
+	response chan string
+	r        *bufio.Reader
+	w        io.Writer
 }
 
 func Create(addr string) *Imap {
-    log.Printf("connecting...")
-    conn, err := tls.Dial("tcp", addr, nil)
-    check(err)
-    log.Printf("connected!")
+	log.Printf("connecting...")
+	conn, err := tls.Dial("tcp", addr, nil)
+	check(err)
+	log.Printf("connected!")
 
-    var w io.Writer = conn
-    var r io.Reader = conn
+	var w io.Writer = conn
+	var r io.Reader = conn
 
-    im := &Imap {
-        addr:       addr,
-        conn:       conn,
-        r:          bufio.NewReader(r),
-        w:          w,
-    }
+	im := &Imap{
+		addr: addr,
+		conn: conn,
+		r:    bufio.NewReader(r),
+		w:    w,
+	}
 
-    return im
+	return im
 }
 
 func (self *Imap) Login(id string, pass string, mail string) {
-    self.userId = id
-    self.passward = pass
-    self.mailBox = mail 
+	self.userId = id
+	self.passward = pass
+	self.mailBox = mail
 
-    ch := make(chan string)
-    self.write(fmt.Sprintf("? LOGIN %s %s", id, pass))
-    go self.getStatus(ch)
-    if status := <- ch; status != "OK" {
-        panic("login error")
-    }
+	ch := make(chan string)
+	self.write(fmt.Sprintf("? LOGIN %s %s", id, pass))
+	go self.getStatus(ch)
+	if status := <-ch; status != "OK" {
+		panic("login error")
+	}
 
-    self.write(fmt.Sprintf("? SELECT %s", mail))
-    go self.getStatus(ch)
-    if status := <- ch; status != "OK" {
-        panic("select error")
-    }
-    log.Printf("login")
+	self.write(fmt.Sprintf("? SELECT %s", mail))
+	go self.getStatus(ch)
+	if status := <-ch; status != "OK" {
+		panic("select error")
+	}
+	log.Printf("login")
 }
 
 func (self *Imap) Logout() {
-    self.write("? LOGOUT")
-    log.Printf("logout")
-    self.conn.Close()
-    log.Printf("close conection")
+	self.write("? LOGOUT")
+	log.Printf("logout")
+	self.conn.Close()
+	log.Printf("close conection")
 }
 
 func (self *Imap) Listen(ch chan string) {
-    self.write("? IDLE")
-    go self.read(ch)
+	self.write("? IDLE")
+	go self.read(ch)
 }
 
 func check(err error) {
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func (self *Imap) write(message string) {
-    n, _ := self.w.Write([]byte(message + "\r\n"))
-    log.Printf("client: wrote %q (%d bytes)", message, n)
+	n, _ := self.w.Write([]byte(message + "\r\n"))
+	log.Printf("client: wrote %q (%d bytes)", message, n)
 }
 
 func (self *Imap) getStatus(ch chan string) {
-    for {
-        token, err := self.r.ReadString(' ')
-        check(err)
+	for {
+		token, err := self.r.ReadString(' ')
+		check(err)
 
-        switch token {
-        case "? ":
-            token, err := self.r.ReadString(' ')
-            check(err)
-            status := strings.TrimRight(token, " ")
-            ch <- status
-            self.readToEOL()
-            return
-        default:
-            self.readToEOL()
-        }
-    }
+		switch token {
+		case "? ":
+			token, err := self.r.ReadString(' ')
+			check(err)
+			status := strings.TrimRight(token, " ")
+			ch <- status
+			self.readToEOL()
+			return
+		default:
+			self.readToEOL()
+		}
+	}
 }
 
 func (self *Imap) read(ch chan string) {
-    self.response = ch
-    for {
-        token, err := self.r.ReadString(' ')
-        if err != nil {
-            self.response <- "close"
-            return
-        }
+	self.response = ch
+	for {
+		token, err := self.r.ReadString(' ')
+		if err != nil {
+			self.response <- "close"
+			return
+		}
 
-        switch token {
-        case "* ":
-            // FETCHコマンド以外実行されない前提
-            self.readFetch()
-        case "+ ":
-            // IDLE
-            self.readToEOL()
-            self.idle()
-        default:
-            // その他はエラー処理なぞせず捨てる
-            self.readToEOL()
-        }
-    }
+		switch token {
+		case "* ":
+			// FETCHコマンド以外実行されない前提
+			self.readFetch()
+		case "+ ":
+			// IDLE
+			self.readToEOL()
+			self.idle()
+		default:
+			// その他はエラー処理なぞせず捨てる
+			self.readToEOL()
+		}
+	}
 }
 
 func (self *Imap) readToEOL() {
-    _, _, err := self.r.ReadLine()
-    check(err)
+	_, _, err := self.r.ReadLine()
+	check(err)
 }
 
 func (self *Imap) readFetch() {
-    _, err := self.r.ReadString(' ')
-    check(err)
-    token, err := self.r.ReadString(' ')
-    check(err)
-    switch token {
-    case "FETCH ":
-        self.readToEOL()
-        // BODYを読み込む
-        encode_text, err := self.r.ReadString(')')
-        check(err)
-        encode_text = strings.TrimRight(encode_text, ")")
-        // crlfでいくつかに区切られているので結合する
-        encode_text = strings.Replace(string(encode_text), "\r\n", "", -1)
-        self.response  <- string(encode_text)
-        self.readToEOL()
-    default:
-        self.readToEOL()
-    }
+	_, err := self.r.ReadString(' ')
+	check(err)
+	token, err := self.r.ReadString(' ')
+	check(err)
+	switch token {
+	case "FETCH ":
+		self.readToEOL()
+		// BODYを読み込む
+		encode_text, err := self.r.ReadString(')')
+		check(err)
+		encode_text = strings.TrimRight(encode_text, ")")
+		// crlfでいくつかに区切られているので結合する
+		encode_text = strings.Replace(string(encode_text), "\r\n", "", -1)
+		self.response <- string(encode_text)
+		self.readToEOL()
+	default:
+		self.readToEOL()
+	}
 }
 
 func (self *Imap) idle() {
-    log.Printf("start idle...")
-    for {
-        token, err := self.r.ReadString(' ')
-        if err != nil {
-            log.Printf("idle EOF")
-            return
-        }
-        switch token {
-        case "* ":
-            num, err := self.r.ReadString(' ')
-            check(err)
-            num = strings.TrimRight(num, " ")
-            log.Printf(string(num))
+	log.Printf("start idle...")
+	for {
+		token, err := self.r.ReadString(' ')
+		if err != nil {
+			log.Printf("idle EOF")
+			return
+		}
+		switch token {
+		case "* ":
+			num, err := self.r.ReadString(' ')
+			check(err)
+			num = strings.TrimRight(num, " ")
+			log.Printf(string(num))
 
-            token, _, err := self.r.ReadLine()
-            if string(token) != "EXISTS" {
-                log.Printf(string(token))
-                continue
-            }         
-            // IDLEしているgoroutineを止めないために新しいgroutineで実行する
-            go self.notify(num)
-        default:
-            self.readToEOL()
-        }
-    }
+			token, _, err := self.r.ReadLine()
+			if string(token) != "EXISTS" {
+				log.Printf(string(token))
+				continue
+			}
+			// IDLEしているgoroutineを止めないために新しいgroutineで実行する
+			go self.notify(num)
+		default:
+			self.readToEOL()
+		}
+	}
 }
 
 func (self *Imap) notify(number string) {
-    // EXISTSを検出したらメール本文をチャネルに通知する
-    im := Create(self.addr)
-    im.Login(self.userId, self.passward, self.mailBox)
-    im.write("? FETCH " + number + " BODY[1]")
-    
-    ch := make(chan string)
-    go im.read(ch)
-    response := <- ch
-    
-    im.Logout()
+	// EXISTSを検出したらメール本文をチャネルに通知する
+	im := Create(self.addr)
+	im.Login(self.userId, self.passward, self.mailBox)
+	im.write("? FETCH " + number + " BODY[1]")
 
-    self.response <- response
+	ch := make(chan string)
+	go im.read(ch)
+	response := <-ch
+
+	im.Logout()
+
+	self.response <- response
 }
--- main.go
+++ main.go
@@ -1,92 +1,93 @@
 package main
+
 import (
-    "io/ioutil"
-    "log"
-    "gopkg.in/yaml.v2"
-    "./imap"
-    "encoding/base64"
-    "regexp"
-    "github.com/bluele/slack"
-    "fmt"
+	"./imap"
+	"encoding/base64"
+	"fmt"
+	"github.com/bluele/slack"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"regexp"
 )
 
 type Config struct {
-    UserId          string
-    Passward        string
-    MailBox         string
-    SlackInfo       []SlackInfo
+	UserId    string
+	Passward  string
+	MailBox   string
+	SlackInfo []SlackInfo
 }
 
 type SlackInfo struct {
-    SlackToken      string
-    SlackName       string
-    SlackIconUrl    string
-    SlackChannel    string
+	SlackToken   string
+	SlackName    string
+	SlackIconUrl string
+	SlackChannel string
 }
 
 func check(err error) {
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func notify(message string) {
-    for _, row := range config.SlackInfo {
-        hook := slack.NewWebHook(row.SlackToken)
-        err := hook.PostMessage(&slack.WebHookPostPayload{
-            Attachments: []*slack.Attachment{
-                {Text: message, Color: "danger"},
-            },
-            Channel: row.SlackChannel,
-            Username: row.SlackName,
-            IconUrl: row.SlackIconUrl,
-        })
-        check(err)
-    }
+	for _, row := range config.SlackInfo {
+		hook := slack.NewWebHook(row.SlackToken)
+		err := hook.PostMessage(&slack.WebHookPostPayload{
+			Attachments: []*slack.Attachment{
+				{Text: message, Color: "danger"},
+			},
+			Channel:  row.SlackChannel,
+			Username: row.SlackName,
+			IconUrl:  row.SlackIconUrl,
+		})
+		check(err)
+	}
 }
 
 func responseLoop(im *imap.Imap) {
-    ch := make(chan string)
-    im.Listen(ch)
+	ch := make(chan string)
+	im.Listen(ch)
 
-    for {
-        response := <- ch
-        switch response {
-        case "close":
-            im.Logout()
-            return
-        default:
-            decode, err := base64.StdEncoding.DecodeString(response)
-            decode_text := string(decode)
-            if err != nil {
-                decode_text = response
-            }
-            log.Printf(string(decode_text))
+	for {
+		response := <-ch
+		switch response {
+		case "close":
+			im.Logout()
+			return
+		default:
+			decode, err := base64.StdEncoding.DecodeString(response)
+			decode_text := string(decode)
+			if err != nil {
+				decode_text = response
+			}
+			log.Printf(string(decode_text))
 
-            assined := regexp.MustCompile("合計: (.*)\r\n")
-            group := assined.FindStringSubmatch(string(decode_text))
-            if group != nil {
-                log.Printf(group[1])
-                notify(fmt.Sprintf("私は%s課金しました", group[1]))
-            } else {
-                notify("私は課金しました?")
-            }
-        }
-    }
+			assined := regexp.MustCompile("合計: (.*)\r\n")
+			group := assined.FindStringSubmatch(string(decode_text))
+			if group != nil {
+				log.Printf(group[1])
+				notify(fmt.Sprintf("私は%s課金しました", group[1]))
+			} else {
+				notify("私は課金しました?")
+			}
+		}
+	}
 }
 
 var config Config
 
 func main() {
-    log.SetFlags(log.LstdFlags | log.Lshortfile) 
-    buf, err := ioutil.ReadFile("config.yml")
-    check(err)
-    err = yaml.Unmarshal(buf, &config)
-    check(err)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	buf, err := ioutil.ReadFile("config.yml")
+	check(err)
+	err = yaml.Unmarshal(buf, &config)
+	check(err)
 
-    for {
-        im := imap.Create("imap.gmail.com:993")
-        im.Login(config.UserId, config.Passward, config.MailBox)
-        responseLoop(im)
-    }
+	for {
+		im := imap.Create("imap.gmail.com:993")
+		im.Login(config.UserId, config.Passward, config.MailBox)
+		responseLoop(im)
+	}
 }
